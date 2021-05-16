92b2f41c46595b5f90f47ec93d062414e2fe418f
Vishnu Sivan <codemaker2015@gmail.com>
2021-05-16 09:33:18 +0530

    initial commit

---
+++ app/src/main/java/com/example/arcoreaugmentedimage/MainActivity.kt
@@ -0,0 +1,214 @@
+package com.example.arcoreaugmentedimage
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.*
+import com.google.ar.core.exceptions.*
+import com.google.ar.sceneform.FrameTime
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.rendering.ViewRenderable
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.util.concurrent.CompletableFuture
+import java.util.function.Consumer
+import java.util.function.Function
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private var installRequested: Boolean = false
+    private var session: Session? = null
+    private var shouldConfigureSession = false
+    private val messageSnackbarHelper = SnackbarHelper()
+    internal lateinit var dataView: CompletableFuture<ViewRenderable>
+    var sensorsMap = HashMap<String, ViewRenderable>()
+    val degree : Char = '\u00B0'
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initializeSceneView()
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, results: IntArray) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(
+                this, "Camera permissions are needed to run this application", Toast.LENGTH_LONG
+            )
+                .show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus)
+    }
+
+    private fun initializeSceneView() {
+
+//        makeInfoView()
+        arSceneView.scene.addOnUpdateListener(this::onUpdateFrame)
+
+    }
+
+
+    private fun onUpdateFrame(frameTime: FrameTime) {
+        frameTime.toString()
+        val frame = arSceneView.arFrame
+        val updatedAugmentedImages = frame?.getUpdatedTrackables(AugmentedImage::class.java)
+
+        if (updatedAugmentedImages != null) {
+            for (augmentedImage in updatedAugmentedImages) {
+                if (augmentedImage.trackingState == TrackingState.TRACKING) {
+                    // Check camera image matches our reference image
+
+                    if (augmentedImage.name == "marker") {
+                        //createRenderable(augmentedImage)
+                        //takePhoto(augmentedImage)
+                        ModelRenderable.builder()
+                            .setSource(this, R.raw.cube)
+                            .build()
+                            .thenAccept(Consumer<ModelRenderable> { renderable: ModelRenderable? -> onRenderableLoaded(renderable) })
+                            .exceptionally(
+                                Function<Throwable, Void?> { throwable: Throwable? ->
+                                    val toast = Toast.makeText(
+                                        this,
+                                        "Unable to load andy renderable",
+                                        Toast.LENGTH_LONG
+                                    )
+                                    toast.setGravity(Gravity.CENTER, 0, 0)
+                                    toast.show()
+                                    null
+                                })
+                    }
+
+                }
+            }
+        }
+    }
+
+    fun onRenderableLoaded(model: Renderable?) {
+        val modelNode = Node()
+        modelNode.setRenderable(model)
+        arSceneView.scene.addChild(modelNode)
+        modelNode.localPosition = Vector3(0F, 0F, -1F)
+    }
+
+    private fun setupAugmentedImageDb(config: Config): Boolean {
+        val augmentedImageDatabase: AugmentedImageDatabase
+
+        val augmentedImageBitmap = loadAugmentedImage() ?: return false
+
+        augmentedImageDatabase = AugmentedImageDatabase(session)
+        augmentedImageDatabase.addImage("marker", augmentedImageBitmap)
+
+        config.augmentedImageDatabase = augmentedImageDatabase
+        return true
+    }
+
+    private fun loadAugmentedImage(): Bitmap? {
+        try {
+            assets.open("marker.jpg").use { `is` -> return BitmapFactory.decodeStream(`is`) }
+        } catch (e: IOException) {
+            Log.e(TAG, "IO exception loading augmented image bitmap.", e)
+        }
+
+        return null
+    }
+
+    private fun configureSession() {
+        val config = Config(session)
+        config.focusMode = Config.FocusMode.AUTO
+        if (!setupAugmentedImageDb(config)) {
+            messageSnackbarHelper.showError(this, "Could not setup augmented image database")
+        }
+        config.updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
+        session!!.configure(config)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (session == null) {
+            var exception: Exception? = null
+            var message: String? = null
+            try {
+                when (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
+                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                        installRequested = true
+                        return
+                    }
+                    ArCoreApk.InstallStatus.INSTALLED -> {
+                    }
+                }
+
+                // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+                // permission on Android M and above, now is a good time to ask the user for it.
+                if (!CameraPermissionHelper.hasCameraPermission(this)) {
+                    CameraPermissionHelper.requestCameraPermission(this)
+                    return
+                }
+
+                session = Session(/* context = */this)
+            } catch (e: UnavailableArcoreNotInstalledException) {
+                message = "Please install ARCore"
+                exception = e
+            } catch (e: UnavailableUserDeclinedInstallationException) {
+                message = "Please install ARCore"
+                exception = e
+            } catch (e: UnavailableApkTooOldException) {
+                message = "Please update ARCore"
+                exception = e
+            } catch (e: UnavailableSdkTooOldException) {
+                message = "Please update this app"
+                exception = e
+            } catch (e: Exception) {
+                message = "This device does not support AR"
+                exception = e
+            }
+
+            if (message != null) {
+                messageSnackbarHelper.showError(this, message)
+                Log.e(TAG, "Exception creating session", exception)
+                return
+            }
+
+            shouldConfigureSession = true
+        }
+
+        if (shouldConfigureSession) {
+            configureSession()
+            shouldConfigureSession = false
+            arSceneView.setupSession(session)
+        }
+
+        // Note that order matters - see the note in onPause(), the reverse applies here.
+        try {
+            session!!.resume()
+            arSceneView.resume()
+        } catch (e: CameraNotAvailableException) {
+            // In some cases (such as another camera app launching) the camera may be given to
+            // a different app instead. Handle this properly by showing a message and recreate the
+            // session at the next iteration.
+            messageSnackbarHelper.showError(this, "Camera not available. Please restart the app.")
+            session = null
+            return
+        }
+
+    }
+
+}
